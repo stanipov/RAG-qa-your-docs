ffaf0e739798c755c81583b8f9c7de0ca2403133
Stanislav Filippov <stanislav.filippov@gmail.com>
2024-03-15 11:09:49 -0400

    Added example to parse documents using Semantic Splitter

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from langchain.schema import Document
 import json
 from typing import Iterable
 import os
+from tqdm.auto import tqdm
+import numpy as np
 
 def save_docs_to_jsonl(array:Iterable[Document], file_path:str)->None:
     with open(file_path, 'w') as jsonl_file:
@@ -34,7 +36,7 @@ def uniquify(path):
 
     return path
     
-def gen_report(ans, docs)-> str:
+def gen_report(question, ans, docs)-> str:
     """
     Generates a joint report made from the generated response and the provided documents.
     Note that this helper is not universal. My current documents have refernce/id in the beginning of each.
@@ -60,3 +62,44 @@ These are the contens:
 {extracted_docs_s}
         """
     return report
+#======================================================================================
+#
+#  Parsing documents
+#
+#======================================================================================
+def recursive_semant_splitter(docs: Iterable[Document], splitter, max_len:int, depth:int=2) -> Iterable[Document]:
+    """ A simple iterative splitting using semantic splitter. Only documents larger than max_len will be splitted """
+    first_split = []
+    for dc in tqdm(docs, leave=True):
+        first_split += splitter.transform_documents([dc])
+
+    sem_split_docs = first_split
+    if depth > 1:
+        for _ in tqdm(range(depth)):
+            len_dist = count_lengths(sem_split_docs)
+            if np.max(len_dist) <= max_len:
+                break
+            sem_split_docs = _split_semant_rec(sem_split_docs, splitter, max_len)
+    return sem_split_docs
+
+
+def _split_semant_rec(docs, splitter, max_len):
+    """ A helper for  recursive_semant_splitter()"""
+    docs2split = []
+    unsplitted = []
+    for dc in docs:
+        if len(dc.page_content) > max_len:
+            docs2split.append(dc)
+        else:
+            unsplitted += [dc]
+    t = splitter.transform_documents(docs2split)
+    return unsplitted + t
+
+
+def count_lengths(docs):
+    """ Counts lenghts of all documents """
+    lens = []
+    for doc in docs:
+        lens.append(len(doc.page_content))
+    return np.array(lens, dtype=int)
+
